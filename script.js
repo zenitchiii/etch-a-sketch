db7d8a215118b32ac316c82abb78698a58bfc81c
zenitchiii <105142307+zenitchiii@users.noreply.github.com>
2025-09-27 17:23:21 +0800

    cleaning up the ui

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256); // 0–255
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 const container = document.querySelector("#container");
-const size = 16;
+let size = 16;
 
 for (let i = 0; i<size; i++) {
     for (let j = 0; j<size; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", () =>{
+            square.style.backgroundColor = getRandomColor();
+        });
         container.appendChild(square);
     }
 }
