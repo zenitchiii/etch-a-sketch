4b327291dae09306a03acdeac62a66e80b1e4d58
zenitchiii <105142307+zenitchiii@users.noreply.github.com>
2025-09-27 17:40:56 +0800

    finished

--- script.js
+++ script.js
@@ -6,15 +6,34 @@ function getRandomColor() {
 }
 
 const container = document.querySelector("#container");
-let size = 16;
+const button = document.querySelector("#button")
 
-for (let i = 0; i<size; i++) {
-    for (let j = 0; j<size; j++) {
+function createGrid(size) {
+    container.innerHTML = "";
+    const squareSize = 400/size;
+
+    for (let i = 0; i< size*size; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.addEventListener("mouseenter", () =>{
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", () => {
             square.style.backgroundColor = getRandomColor();
         });
         container.appendChild(square);
     }
 }
+
+createGrid(16);
+
+button.addEventListener("click", () => {
+    let input = prompt("Enter squares per side (max: 100)");
+    let num = parseInt(input);
+
+    if (num > 0 && num <= 100 ) {
+        createGrid(num);
+    } else {
+        alert("Please enter a number within 1 - 100");
+    }
+});
